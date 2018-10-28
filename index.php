8f667f08dd2aa7919b3527a545b97023be5929b4
alakratt <alakratt@gmail.com>
2018-10-28 07:40:42 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php 
+		$isVisible = 'style=\'display:none\'';
+		if ($_POST) {
+			
+			if (strlen(trim($_POST['email'])) > 0) {
+				$email = $_POST['email'];
+				$isVisible = 'style=\'display:block\'';
+				header("Location: welcome.php"); 
+				exit;/* Redirect browser */
+
+/* Make sure that code below does not get executed when we redirect. */
+//exit;
+				/*var_dump($_POST['email']);
+				$_POST = array();
+*/				//$_POST['email'] = null;
+			}
+		}
+		/*echo $_POST['email'];
+		echo strlen(trim($_POST['email']));*/
+		?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<title>PHP Form</title>
+</head>
+<body>
+	
+	<div class="container">
+
+		<form method="post">
+			  <div class="form-group">
+			    <label for="email">User ID</label>
+			    <input type="email" class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" placeholder="name@email.com" required
+			    	value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>">
+			    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+			  </div>
+			  <div class="form-group">
+			    <label for="exampleInputPassword1">Password</label>
+			    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
+			  </div>
+			  <div class="form-check">
+			    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+			    <label class="form-check-label" for="exampleCheck1">Remember me</label>
+			  </div>
+			  <button type="submit" class="btn btn-primary">Log in</button>
+		</form>
+<div id="notif" class="alert alert-primary" <?php echo $isVisible; ?> role="alert">
+  Yeyyy <?php echo $_POST['email'] ?> !!!
+</div>
+	</div>
+
+</body>
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script>
+  	$(document).ready(function (){
+  		$('#notif').fadeOut(3000);
+  	});
+  	
+  </script>
+</html>
---
+++ welcome.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<title></title>
+</head>
+<body>
+	<div class="container">
+		
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Features</a>
+      <a class="nav-item nav-link" href="#">Pricing</a>
+      <a class="nav-item nav-link disabled" href="#">Disabled</a>
+    </div>
+  </div>
+</nav>
+
+		<div class="alert alert-success" role="alert">
+  <h4 class="alert-heading">Well done <?php echo $_POST["email"]; ?>!</h4>
+  <p>You've successfully tested my first php form. I know I know, it's really simple but um.. I'm learning so.... <?= $_POST['email']; ?></p>
+  <hr>
+  <p class="mb-0">Really, it's just that</p>
+</div>
+
+<a class="btn btn-success" href="index.php">Back</a>
+	</div>
+
+</body>
+</html>
